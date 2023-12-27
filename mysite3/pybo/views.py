beeb03170da3483e91a248ed0b28a1e7ed69a8f8
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-12-27 10:11:42 +0900

    feat: url 맵핑 & detail view

--- mysite3/pybo/views.py
+++ mysite3/pybo/views.py
@@ -4,5 +4,12 @@ from .models import Question
 # Create your views here.
 def index(request):
     question_list = Question.objects.all()
+
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question_list = Question.objects.get(id=question_id)
+    
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_detail.html', context)
