ad56aa9e1888f8eb4cdf1d9d36ad40f953715886
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-12-27 10:14:46 +0900

    refactor: get_object_or_404 implementation

--- mysite3/pybo/views.py
+++ mysite3/pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 # Create your views here.
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
-    question_list = Question.objects.get(id=question_id)
-    
+    question_list = get_object_or_404(Question, pk=question_id)
+
     context = {'question_list': question_list}
     return render(request, 'pybo/question_detail.html', context)
