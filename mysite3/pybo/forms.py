4ddaddcd0d8d63ad4ce794026ce3c22e00d71d67
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-12-28 10:27:14 +0900

    refactor: form.as_p 대신 수작업으로 form 작성 for bootstrap implementation

--- mysite3/pybo/forms.py
+++ mysite3/pybo/forms.py
@@ -5,3 +5,7 @@ class QuestionForm(forms.ModelForm): # forms.ModelForm을 상속받음
     class Meta: # Meta 클래스를 반드시 가져야함
         model = Question
         fields = ['subject', 'content']
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
