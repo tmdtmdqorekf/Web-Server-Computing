c2f4379d65d4fbb36f34a2fd6fcb1bde157abee1
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-02 09:38:12 +0900

    feat: pagination init implementation

--- mysite3/pybo/views.py
+++ mysite3/pybo/views.py
@@ -2,12 +2,23 @@ from django.utils import timezone
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    question_list = Question.objects.all()
+    # localhost:8000/pybo/?page=1 처럼
+    # GET 방식으로 호출된 URL에서 page 가져올 때 사용
+    page = request.GET.get('page', '1')
 
-    context = {'question_list': question_list}
+    question_list = Question.objects.all()
+    
+    # 페이지당 10개씩 보여주기
+    paginator = Paginator(question_list, 10)
+    # 요청된 페이지에 해당되는 페이징 객체 생성
+    # 데이터 전체 조회 x, 해당 페이지의 데이터만 조회하도록 쿼리가 변경됨
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
