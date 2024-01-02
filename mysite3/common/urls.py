7f1abfc65284d023ea451a4c21a46fb9e1384c24
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-02 10:27:20 +0900

    feat: login init implementation with common app

---
+++ mysite3/common/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+app_name = 'common'
+
+urlpatterns = [
+    # 따로 template_name 추가하면 registration 폴더가 아닌
+    # common 폴더에서 login.html 참조함
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+]
