ee9020f94dfed9f1bc21f69bcfb9d987c2065638
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-02 10:36:16 +0900

    fix: INSTALLED_APPS common 앱 등록 & 형태 수정

--- mysite3/config/settings.py
+++ mysite3/config/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
+    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pybo'
-    'common'
 ]
 
 MIDDLEWARE = [
