fc03e1ebd9697cd66041662d0ca77916a776d0dc
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-02 11:12:05 +0900

    feat: logout implementation & GET->POST fix

--- mysite3/common/urls.py
+++ mysite3/common/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # 따로 template_name 추가하면 registration 폴더가 아닌
     # common 폴더에서 login.html 참조함
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
