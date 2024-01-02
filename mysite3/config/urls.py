4b28678232ad22e3ea317a7234df0cfc7130c430
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-02 10:54:28 +0900

    feat: login 성공 시 url 설정

--- mysite3/config/urls.py
+++ mysite3/config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
+    path('', views.index, name='index')
 ]
