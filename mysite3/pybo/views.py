f96cd7085c03841954d0b5f9d3acc5e2b2bd1a06
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-07 16:30:01 +0900

    feat: 로그아웃 시 답변 등록 막기

--- mysite3/pybo/views.py
+++ mysite3/pybo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question=get_object_or_404(Question, pk=question_id)
 
@@ -34,6 +36,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.created_date = timezone.now()
             answer.question = question
             answer.save()
@@ -44,11 +47,13 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.created_date = timezone.now()
             question.save()
             return redirect('pybo:index')
