4eb5621fb0a2c0356d4ed008842c435f6650ca08
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-01-09 10:16:25 +0900

    feat: search implementation

--- mysite3/pybo/views/base_views.py
+++ mysite3/pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -7,8 +8,18 @@ def index(request):
     # localhost:8000/pybo/?page=1 처럼
     # GET 방식으로 호출된 URL에서 page 가져올 때 사용
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')  # 검색어
 
     question_list = Question.objects.all()
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     
     # 페이지당 10개씩 보여주기
     paginator = Paginator(question_list, 10)
@@ -16,7 +27,7 @@ def index(request):
     # 데이터 전체 조회 x, 해당 페이지의 데이터만 조회하도록 쿼리가 변경됨
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
